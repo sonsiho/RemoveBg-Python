1b24c99f33ccef3edfb00dd09efb657240f1d93d
sonsiho <anson.hs38@gmail.com>
2024-02-29 23:01:45 +0700

    up

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ ae = 10
 az = 1000
 model_name = "u2netp"
 
-with open('data/HinhChuKy2.png', 'rb') as fh:
+with open('data/HinhChuKy6.png', 'rb') as fh:
     buf = fh.read()
 
 img_remove = bg.remove(buf, model_name, alpha_matting)
 
 # img_remove2 = remove(img_remove)
 # print(img_remove2)
-plt.imshow(img_remove)
-plt.show()
+# plt.imshow(img_remove)
+# plt.show()
+
+img_remove.save('data/Results/HinhChuKy6_rembg.png')
